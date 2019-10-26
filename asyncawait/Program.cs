48ff51f65868fa3dd364eb548d399abbc23b8f43
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2019-10-26 19:36:12 +0200

    add readme

--- asyncawait/Program.cs
+++ asyncawait/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+//// https://www.youtube.com/watch?v=6_GTdR0gBVE
 
 namespace asyncawait
 {
@@ -10,6 +12,51 @@ namespace asyncawait
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("hello");
+            var worker = new Worker();
+            worker.DoWork();
+
+            while (!worker.Iscomplete)
+            {
+                Console.Write(".");
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+
+    internal class Worker
+    {
+
+        public bool Iscomplete { get; internal set; }
+   
+        public Worker()
+        {
+
+
+        }
+
+        internal async void DoWork()
+        {
+            this.Iscomplete = false;
+
+            Console.WriteLine("doing Work");
+
+            await LongOperation();
+
+            this.Iscomplete = true;
+        }
+
+        private Task LongOperation()
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("working");
+                Thread.Sleep(2000);
+
+            });
         }
     }
 }
